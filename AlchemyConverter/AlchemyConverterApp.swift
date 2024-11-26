1caaa46298f706a4a329e08ff018f68eeab4b311
Masume Çetin <masume.cetin@veripark.com>
2024-11-26 18:48:49 +0300

    Initial Commit

---
+++ AlchemyConverter/AlchemyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlchemyConverterApp.swift
+//  AlchemyConverter
+//
+//  Created by Masume Çetin on 26.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlchemyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlchemyConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlchemyConverter
+//
+//  Created by Masume Çetin on 26.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
